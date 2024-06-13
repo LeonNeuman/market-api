6a9942ffcc7b285e6f465b16f105396867c65f67
Leon <leon.neumaann@gmail.com>
2024-06-13 10:35:43 -0300

    c0 - Criacao das Classes Anemicas

---
+++ APICatalogo/Controllers/Models/Categorias.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Controllers.Models;
+
+public class Categorias
+{
+    public int CategoriasId { get; set; }
+    public string? Nome { get; }
+    public string? ImagemUrl { get; set; } 
+}
